51bc7a5b81365386e313f1eaa527a115af5828cc
petr <p.razumkin@yandex.ru>
2022-02-10 20:57:32 +0300

    HW P3_3

--- src/ru/progwards/java1/lessons/io1/CharFilter.java
+++ src/ru/progwards/java1/lessons/io1/CharFilter.java
@@ -3,21 +3,33 @@ package ru.progwards.java1.lessons.io1;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class CharFilter {
 
     public static void filterFile(String inFileName, String outFileName, String filter) throws IOException {
         FileReader fileReader = new FileReader(inFileName);
         FileWriter fileWriter = new FileWriter(outFileName);
-        char[] stringFilter = filter.toCharArray();
+
         try {
-            for (int ch; (ch = fileReader.read()) >= 0; ) {
-                for (char c : stringFilter) {
-                    if (ch != c - '0') {
-                        fileWriter.write(ch);
+            Scanner scanner = new Scanner(fileReader);
+            String result = "";
+            while (scanner.hasNextLine()) {
+                String fileText = scanner.nextLine();
+                for (int i = 0; i < fileText.length(); i++) {
+                    String temp = String.valueOf(fileText.charAt(i));
+                    for (int j = 0; j < filter.length(); j++) {
+                        if (fileText.charAt(i) == filter.charAt(j)) {
+                            temp = "";
+                        }
                     }
+                    result += temp;
                 }
             }
+            fileWriter.write(result);
+            scanner.close();
+        } catch (IOException e) {
+           throw new IOException("Файл не найден");
         } finally {
             fileReader.close();
             fileWriter.close();
